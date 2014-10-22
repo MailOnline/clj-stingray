86a552af748d4826e24386568a219439ce53a562
reborg <reborg@reborg.net>
2014-10-22 12:16:31 +0100

    added retrieve of enabled/disabled nodes from a pool

--- src/clj_stingray/core.clj
+++ src/clj_stingray/core.clj
@@ -16,4 +16,26 @@
 (def opts-default (-> {:as :json} (with-basic-auth) (insecure?)))
 
 (defn pools []
-  (:body (client/get (url "api/tm/1.0/config/active/pools") opts-default)))
+  "Retrieves all pools"
+  (get-in (client/get (url "api/tm/1.0/config/active/pools") opts-default) [:body :children]))
+
+(defn pool [name]
+  "Retrieves pool data by name"
+  (get-in (client/get (url (str "api/tm/1.0/config/active/pools/" name)) opts-default) [:body :properties]))
+
+(defn enabled-nodes [name]
+  "Retrieves all enabled nodes in the named pool."
+  (let [pool (pool name)]
+        (get-in pool [:basic :nodes])))
+
+(defn disabled-nodes [name]
+  "Retrieves all disabled nodes in the named pool."
+  (let [pool (pool name)]
+        (get-in pool [:basic :disabled])))
+
+(defn nodes [name]
+  "Retrieves all nodes in the named pool."
+  (let [pool (pool name)
+        disabled (disabled-nodes name)
+        enabled (enabled-nodes name)]
+  (into enabled disabled)))
